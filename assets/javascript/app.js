40785a8c0f226273064e6309f7377de0a4274351
TwentyVT <sonatore@amazon.com>
2018-10-14 23:04:42 -0400

    done but with bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,20 +2,23 @@ $(document).ready(function () {
 
     var correctcount = 0;
     var wrongcount = 0;
-    var time = 0;
     var count = 30;
-    var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+    var counter = setInterval(timer, 1000);
+    var resetbutton = $("<button>Reset</button>").click(reset)
+
 
     function timer() {
+
         count = count - 1;
-        if (count = 0) {
+        if (count <= 0) {
             clearInterval(counter);
-            //counter ended, do something here
-            return;
+            $("#rightcount").text("You got " + correctcount + " right!")
+            $("#wrongcount").text("You got " + wrongcount + " wrong!")
+            $("#reset").append(resetbutton)
         }
 
 
-        $("#timer").text(count);
+        $("#timer").text("Time left " + count);
     }
 
     $('input[type=radio][name=answer1]').change(function () {
@@ -24,90 +27,111 @@ $(document).ready(function () {
 
             correctcount++
 
+
             console.log(correctcount)
         }
         else if (this.value !== "two") {
+            
             wrongcount++
+
+   
+
             console.log("Wrong count" + wrongcount)
         }
 
     });
 
-    $('input[type=radio][name=answer1]').change(function () {
+    $('input[type=radio][name=answer2]').change(function () {
 
         if (this.value === "two") {
 
             correctcount++
 
+
+
             console.log(correctcount)
         }
         else if (this.value !== "two") {
             wrongcount++
+
+
+
             console.log("Wrong count" + wrongcount)
         }
     });
 
-    $('input[type=radio][name=answer1]').change(function () {
+    $('input[type=radio][name=answer3]').change(function () {
 
         if (this.value === "two") {
 
             correctcount++
 
+
             console.log(correctcount)
         }
         else if (this.value !== "two") {
             wrongcount++
+
+     
+
             console.log("Wrong count" + wrongcount)
         }
     });
 
-    $('input[type=radio][name=answer1]').change(function () {
+    $('input[type=radio][name=answer4]').change(function () {
 
         if (this.value === "two") {
 
             correctcount++
 
+  
+
+
             console.log(correctcount)
         }
         else if (this.value !== "two") {
             wrongcount++
+
+
             console.log("Wrong count" + wrongcount)
         }
     });
 
-    $('input[type=radio][name=answer1]').change(function () {
+    $('input[type=radio][name=answer5]').change(function () {
 
         if (this.value === "two") {
 
             correctcount++
 
+    
+
             console.log(correctcount)
         }
         else if (this.value !== "two") {
             wrongcount++
+
+
             console.log("Wrong count" + wrongcount)
         }
     });
-    // $('input[type=radio][name=answer1]').change(function() {
 
-    //     if (this.value === "one") {
+    function reset() {
+        correctcount = 0;
+        wrongcount = 0;
+        count = 30;
+        counter = setInterval(timer, 1000);
+        resetbutton = $("<button>Reset</button>").click(reset)
+        $('input[name="answer1"]').prop('checked', false);
+        $('input[name="answer2"]').prop('checked', false);
+        $('input[name="answer3"]').prop('checked', false);
+        $('input[name="answer4"]').prop('checked', false);
+        $('input[name="answer5"]').prop('checked', false);
+        $("#rightcount").empty()
+        $("#wrongcount").empty()
+        $("#reset").empty()
 
-    //     correctcount++
 
-    //     console.log(correctcount)
-    // }
 
-    // var question1a = $('<input type="radio" name="rbtnCount" />');
-    // question1a.appendTo('#question1');
-
-    // var question1b = $('<input type="radio" name="rbtnCount" />');
-    // question1b.appendTo('#question1');
-
-    // var question1c = $('<input type="radio" name="rbtnCount" />');
-    // question1c.appendTo('#question1');
-
-    // var question1d = $('<input type="radio" name="rbtnCount" />');
-    // question1d.appendTo('#question1');
-
+    }
 });
 
