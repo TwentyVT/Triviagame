9bc381848fb7fe41f90b67fee099115edc463e1f
TwentyVT <sonatore@amazon.com>
2018-10-14 20:43:18 -0400

    timer done, questions ready

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,113 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-var correctcount = 0;
-var wrongcount = 0;
+    var correctcount = 0;
+    var wrongcount = 0;
+    var time = 0;
+    var count = 30;
+    var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
 
-$('input[type=radio][name=answer2]').change(function() {
+    function timer() {
+        count = count - 1;
+        if (count = 0) {
+            clearInterval(counter);
+            //counter ended, do something here
+            return;
+        }
 
-if (this.value === "two") {
 
-correctcount++
+        $("#timer").text(count);
+    }
 
-console.log(correctcount)
-}
-else if (this.value !== "two") {
-    wrongcount++
-    console.log("Wrong count" + wrongcount)
-}
+    $('input[type=radio][name=answer1]').change(function () {
+
+        if (this.value === "two") {
+
+            correctcount++
+
+            console.log(correctcount)
+        }
+        else if (this.value !== "two") {
+            wrongcount++
+            console.log("Wrong count" + wrongcount)
+        }
+
+    });
+
+    $('input[type=radio][name=answer1]').change(function () {
+
+        if (this.value === "two") {
+
+            correctcount++
+
+            console.log(correctcount)
+        }
+        else if (this.value !== "two") {
+            wrongcount++
+            console.log("Wrong count" + wrongcount)
+        }
+    });
+
+    $('input[type=radio][name=answer1]').change(function () {
+
+        if (this.value === "two") {
+
+            correctcount++
+
+            console.log(correctcount)
+        }
+        else if (this.value !== "two") {
+            wrongcount++
+            console.log("Wrong count" + wrongcount)
+        }
+    });
+
+    $('input[type=radio][name=answer1]').change(function () {
+
+        if (this.value === "two") {
+
+            correctcount++
+
+            console.log(correctcount)
+        }
+        else if (this.value !== "two") {
+            wrongcount++
+            console.log("Wrong count" + wrongcount)
+        }
+    });
+
+    $('input[type=radio][name=answer1]').change(function () {
+
+        if (this.value === "two") {
+
+            correctcount++
+
+            console.log(correctcount)
+        }
+        else if (this.value !== "two") {
+            wrongcount++
+            console.log("Wrong count" + wrongcount)
+        }
+    });
+    // $('input[type=radio][name=answer1]').change(function() {
+
+    //     if (this.value === "one") {
+
+    //     correctcount++
+
+    //     console.log(correctcount)
+    // }
+
+    // var question1a = $('<input type="radio" name="rbtnCount" />');
+    // question1a.appendTo('#question1');
+
+    // var question1b = $('<input type="radio" name="rbtnCount" />');
+    // question1b.appendTo('#question1');
+
+    // var question1c = $('<input type="radio" name="rbtnCount" />');
+    // question1c.appendTo('#question1');
+
+    // var question1d = $('<input type="radio" name="rbtnCount" />');
+    // question1d.appendTo('#question1');
 
 });
 
-$('input[type=radio][name=answer1]').change(function() {
-
-    if (this.value === "one") {
-    
-    correctcount++
-    
-    console.log(correctcount)
-}
-
-// var question1a = $('<input type="radio" name="rbtnCount" />');
-// question1a.appendTo('#question1');
-
-// var question1b = $('<input type="radio" name="rbtnCount" />');
-// question1b.appendTo('#question1');
-
-// var question1c = $('<input type="radio" name="rbtnCount" />');
-// question1c.appendTo('#question1');
-
-// var question1d = $('<input type="radio" name="rbtnCount" />');
-// question1d.appendTo('#question1');
-
-});
-});
